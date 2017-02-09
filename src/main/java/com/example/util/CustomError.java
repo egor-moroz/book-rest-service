54a1bae1cf187f7d78f84fd2130a0a969c54b1e1
egor-moroz <egorlex@gmail.com>
2017-02-09 23:03:40 +0200

    update util

---
+++ src/main/java/com/example/util/CustomError.java
@@ -0,0 +1,16 @@
+package com.example.util;
+
+
+public class CustomError {
+
+    private String errorMessage;
+
+    public CustomError(String errorMessage){
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+}
